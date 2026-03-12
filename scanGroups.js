ae76de0e43e0f435ced251ff228955854d729465
Leandro Pinto <leandroampinto@gmail.com>
2026-03-12 17:53:40 +0000

    Removendo os filhos repetidos

--- scanGroups.js
+++ scanGroups.js
@@ -1,81 +1,147 @@
-const fs = require("fs");
-const path = require("path");
+const fs = require("node:fs/promises");
+const path = require("node:path");
 
-const groupNamesByGroupId = {};
-try {
-    const groupFile = fs.readFileSync("/etc/group", "utf8");
+const readGroupNamesByGroupId = async () => {
+    const groupNamesByGroupId = {};
+    try {
+        const groupFile = await fs.readFile("/etc/group", "utf8");
 
-    groupFile.split("\n").forEach(line => {
-        if (!line) return;
+        groupFile.split("\n").forEach(line => {
+            if (!line) {
+                return;
+            }
 
-        const parts = line.split(":");                // cria algo como:
-        const name = parts[0];                        //   {
-        const gid = parseInt(parts[2], 10);           //        0: "root",
-        //        27: "sudo", 
-        groupNamesByGroupId[gid] = name;                         //        1000: "glauco"
-    });                                               //   }
+            const parts = line.split(":");
+            const name = parts[0];
+            const gid = parseInt(parts[2], 10);
+            groupNamesByGroupId[gid] = name;
+        });
 
-} catch (err) {
-    console.error("Erro lendo /etc/group:", err);
+    } catch (err) {
+        console.error("Erro lendo /etc/group:", err);
+        process.exit(1);
+    }
+    return groupNamesByGroupId;
 }
 
 const root = process.argv[2] || ".";
-const groupNamesByPath = {};
 
-function scanDirectory(dir) {
+const scanRootDir = async (rootDir) => {
+    const scan = async (parentPath) => {
+        try {
+            const entries = await fs.readdir(parentPath, { withFileTypes: true });
+            let groupId = null;
+            let isEmpty = true;
+            let saveEntries = false;
+            let pathsAndGroupIds = [];
 
-    const entries = fs.readdirSync(dir, { withFileTypes: true });
-    let groupId = null;
-    let isEmpty = true;
-    let saveEntries = false;
-    let fullPathsAndGroupIds = [];  //path de arquivos e paths de diretorios que scanDirectory retornou != -1 e != null
+            for (const entry of entries) {
+                if (entry.name === "CVS" || entry.name === "CVSROOT") {
+                    continue;
+                }
+                const name = entry.name;
+                const fullPath = path.join(parentPath, name);
+                if (entry.isSymbolicLink() || (!entry.isDirectory() && !entry.isFile())) {
+                    continue;
+                }
 
-    for (const entry of entries) {
+                let entryGroupId;
+                if (entry.isDirectory()) {
+                    entryGroupId = scan(fullPath);
+                } else {
+                    try {
+                        const stats = fs.statSync(fullPath);
+                        entryGroupId = stats.gid;
+                    } catch (e) {
+                        console.error("!", fullPath, e.code);
+                        continue;
+                    }
+                }
+                if (entryGroupId === null) {
+                    continue;
+                }
 
-        const fullPath = path.join(dir, entry.name);
-        if (entry.isSymbolicLink() || (!entry.isDirectory() && !entry.isFile()) || entry.name === "CVS" || entry.name === "CVSROOT") {
-            continue;
-        }
+                pathsAndGroupIds.push([fullPath, entryGroupId]);
+                isEmpty = false;
+                if (groupId === null) {
+                    groupId = entryGroupId;
+                }
 
-        let entryGroupId;
-        if (entry.isDirectory()) {
-            entryGroupId = scanDirectory(fullPath);
-        } else {
-            const stats = fs.statSync(fullPath);
-            entryGroupId = stats.gid;
-        }
-        if (entryGroupId === null) {
-            continue;
-        }
-        if (entryGroupId !== -1 ) {
-            fullPathsAndGroupIds.push([fullPath, entryGroupId]);
-            isEmpty = false;
-        }
-        if (groupId === null && entryGroupId !== -1 ) {
-            groupId = entryGroupId;
-        }
+                if (groupId !== null && groupId !== entryGroupId) {
+                    saveEntries = true;
+                }
+            }
+            if (isEmpty) {
+                return null;
+            }
+            if (saveEntries) {
+                const countByGroupId = {};
+                for (const fullPathAndGroupId of pathsAndGroupIds) {
+                    const [, groupId] = fullPathAndGroupId;
+                    countByGroupId[groupId] = (countByGroupId[groupId] ?? 0) + 1;
+                }
+                let maxGroupId = null;
+                let maxCount = 0;
+                for (const key in countByGroupId) {
+                    const groupId = Number(key);
+                    const count = countByGroupId[groupId];
+                    if (count > maxCount) {
+                        maxCount = count;
+                        maxGroupId = groupId;
+                    }
+                }
+                const maxGroupName = groupNamesByGroupId[maxGroupId];
+                groupsByPath[parentPath] = { groupId: maxGroupId, groupName: maxGroupName };
+                for (const pathAndGroupId of pathsAndGroupIds) {
+                    const [path, groupId] = pathAndGroupId;
+                    const groupName = groupNamesByGroupId[groupId];
+                    groupsByPath[path] = { groupId, groupName };
+                }
+                return maxGroupId;
+            }
 
-        if (groupId !== null && groupId !== entryGroupId) {
-            saveEntries = true;
+            return groupId;
+        } catch (e) {
+            console.error('#', parentPath, e.code);
+            return null;
         }
+    };
+
+    const removeRepetition = async (parentPath, parentGroupId) => {
+        try {
+            const entries = await fs.readdir(parentPath, { withFileTypes: true });
+            for (const entry of entries) {
+                if (entry.name === "CVS" || entry.name === "CVSROOT") {
+                    continue;
+                }
+                const name = entry.name;
+                const entryPath = path.join(parentPath, name);
+                const entryGroup = groupsByPath[entryPath];
+                const { groupId: entryGroupId } = entryGroup;
+                if (entryGroupId === parentGroupId) {
+                    groupsByPath[entryPath] = undefined;
+                }
+                await removeRepetition(entryPath, entryGroupId);
+            }
+        } catch (e) {
+            console.error('#', parentPath, e.code);
+        }
+    };
+
+    const groupNamesByGroupId = await readGroupNamesByGroupId();
+
+    const groupsByPath = {};
+
+    const rootGroupId = scan(rootDir);
+    if (rootGroupId !== null) {
+        const groupName = groupNamesByGroupId[rootGroupId];
+        groupsByPath[rootDir] = { groupId, groupName };
     }
-    if (isEmpty) {
-        return null;
-    }
-    if (saveEntries) {
-        for ( const fullPathAndGroupId of fullPathsAndGroupIds ) {
-            const [fullPath, groupId] = fullPathAndGroupId;
-            groupNamesByPath[fullPath] =  groupNamesByGroupId[groupId];
-        }
-        return -1;
-    }
+    removeRepetition(rootDir, rootGroupId);
 
-    return groupId;
+    return groupsByPath;
 }
 
-const groupId = scanDirectory(root);
-if (groupId !== null && groupId !== -1) {
-    groupNamesByPath[root] =  groupNamesByGroupId[groupId];
-}
+const groupsByPath = scanRootDir(root);
 
-console.log(JSON.stringify(groupNamesByPath, null, 2));
+console.log(JSON.stringify(groupsByPath, null, 2));
